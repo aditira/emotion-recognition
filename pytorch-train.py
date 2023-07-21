58669395e96ec94a0972bfd63bacaae0fa7732cd
Aditira Jamhuri <aditirajamhuri@gmail.com>
2023-07-21 09:08:48 +0700

    feat: add pytorch model training

--- pytorch-train.py
+++ pytorch-train.py
@@ -1,104 +1,119 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim import lr_scheduler
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torchvision import datasets, transforms, models
 import torch.nn.functional as F
 import numpy as np
+import copy
 
-class EmotionRecognitionModel(nn.Module):
-    def __init__(self):
-        super(EmotionRecognitionModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64*1*1, 1024)
-        self.fc2 = nn.Linear(1024, 8)
+# Use pre-trained ResNet and modify the last layer
+model = models.resnet18(pretrained=True)
 
-    def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.avg_pool2d(x, x.shape[2:])
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+# Change last layer to match your number of classes
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 8)  # assuming there are 8 different emotions to be recognized
 
-model = EmotionRecognitionModel()
+# Data augmentation and normalization for training, normalization for validation
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Grayscale(num_output_channels=3),  # ResNet18 requires 3 channel input
+        transforms.RandomHorizontalFlip(),
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Grayscale(num_output_channels=3),
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
 
-from torchsummary import summary
-summary(model, (1, 32, 32), device='cpu')
-
-transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((32, 32)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,), (0.5,))
-])
-
-trainset = datasets.ImageFolder(root='dataset/kaggle', transform=transform)
+trainset = datasets.ImageFolder(root='dataset/kaggle', transform=data_transforms['train'])
 trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
 
-testset = datasets.ImageFolder(root='dataset/kaggle', transform=transform)
-testloader = DataLoader(testset, batch_size=32, shuffle=True)
+valset = datasets.ImageFolder(root='dataset/kaggle', transform=data_transforms['val'])
+valloader = DataLoader(valset, batch_size=32, shuffle=True)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+dataloaders = {'train': trainloader, 'val': valloader}
 
-device = torch.device("cpu")
+# Use GPU if available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 model = model.to(device)
+criterion = nn.CrossEntropyLoss()
 
-loss_values = []
-val_loss_values = []
+# Observe that all parameters are being optimized
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-patience = 5  # Number of epochs to wait for improvement in validation loss
-patience_counter = 0  # Counter to keep track of epochs without improvement
-best_val_loss = np.inf  # Keep track of best validation loss
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-for epoch in range(50):  # loop over the dataset multiple times
+# Number of epochs to train for 
+num_epochs = 25
 
-    # Training phase
-    running_loss = 0.0
-    model.train()
-    for i, data in enumerate(trainloader, 0):
-        inputs, labels = data[0].to(device), data[1].to(device)
+best_model_wts = copy.deepcopy(model.state_dict())
+best_acc = 0.0
 
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+for epoch in range(num_epochs):
+    print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+    print('-' * 10)
 
-        running_loss += loss.item()
+    # Each epoch has a training and validation phase
+    for phase in ['train', 'val']:
+        if phase == 'train':
+            model.train()  # Set model to training mode
+        else:
+            model.eval()   # Set model to evaluate mode
 
-    loss_values.append(running_loss / len(trainloader))
-    print('Epoch %d training loss: %.3f' % (epoch + 1, loss_values[-1]))
+        running_loss = 0.0
+        running_corrects = 0
 
-    # Validation phase
-    val_running_loss = 0.0
-    model.eval()
-    with torch.no_grad():
-        for i, data in enumerate(testloader, 0):
-            inputs, labels = data[0].to(device), data[1].to(device)
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+        # Iterate over data.
+        for inputs, labels in dataloaders[phase]:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
 
-            val_running_loss += loss.item()
-    
-    val_loss_values.append(val_running_loss / len(testloader))
-    print('Epoch %d validation loss: %.3f' % (epoch + 1, val_loss_values[-1]))
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-    # Check for improvement in validation loss
-    if val_loss_values[-1] < best_val_loss:  # Loss improved
-        best_val_loss = val_loss_values[-1]
-        patience_counter = 0  # Reset the counter
-    else:  # Loss did not improve
-        patience_counter += 1  # Increment the counter
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(phase == 'train'):
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
 
-    if patience_counter >= patience:
-        print(f"Stopping training because validation loss has not improved in {patience} epochs.")
-        break
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
 
-print('Finished Training')
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+        if phase == 'train':
+            exp_lr_scheduler.step()
+
+        epoch_loss = running_loss / len(dataloaders[phase].dataset)
+        epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+        print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+        # deep copy the model
+        if phase == 'val' and epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = copy.deepcopy(model.state_dict())
+
+    print()
+
+print('Best val Acc: {:4f}'.format(best_acc))
+
+# load best model weights
+model.load_state_dict(best_model_wts)
+
+# Save model
+torch.save(model, 'model/pytorch/emotion_recognition_model.pth')
